50bb791e12eb3acb475e7ee9e4ca900a9a8f2271
Evelyn <evelynbandeiramesquita@gmail.com>
2023-06-04 12:43:37 -0300

    fix: realod timeline error

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,50 +1,69 @@
-import styled from "styled-components"
-import { SlArrowUp, SlArrowDown } from 'react-icons/sl'
-import { useContext, useState } from "react"
-import { AuthContext } from "../providers/auth"
-import SearchBar from "./SearchBar"
-import { useNavigate } from "react-router-dom"
-import axios from "axios"
+import React, { useContext, useState } from "react";
+import styled from "styled-components";
+import { SlArrowUp, SlArrowDown } from 'react-icons/sl';
+import { AuthContext } from "../providers/auth";
+import SearchBar from "./SearchBar";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function Header({ setReload }) {
-    const navigate = useNavigate()
-    const [menu, setMenu] = useState(false)
-    const { user } = useContext(AuthContext)
+    const navigate = useNavigate();
+    const [menu, setMenu] = useState(false);
+    const { user } = useContext(AuthContext);
 
     function logout() {
-        localStorage.removeItem('userSessionInfoLinkr')
-        const removeSession = axios.delete(`${process.env.REACT_APP_API_URL}/logout/${user.token}`)
-        removeSession.then((answer) => navigate('/'))
-        removeSession.catch((error) => alert(error.response.data))
+        localStorage.removeItem('userSessionInfoLinkr');
+        const removeSession = axios.delete(`${process.env.REACT_APP_API_URL}/logout/${user.token}`);
+        removeSession.then((answer) => navigate('/'));
+        removeSession.catch((error) => alert(error.response.data));
     }
+
     return (
         <HeaderStyle>
             <h1 onClick={() => navigate("/timeline")}>linkr</h1>
             <SearchBar setReload={setReload} />
             <div>
-                {menu ? <SlArrowDown onClick={() => setMenu(!menu)} color="white" size={'25'} cursor="pointer" /> : <SlArrowUp onClick={() => setMenu(!menu)} color="white" size={'25'} cursor="pointer" />}
-
+                {menu ? (
+                    <SlArrowDown
+                        onClick={() => setMenu(!menu)}
+                        color="white"
+                        size={'25'}
+                        cursor="pointer"
+                    />
+                ) : (
+                    <SlArrowUp
+                        onClick={() => setMenu(!menu)}
+                        color="white"
+                        size={'25'}
+                        cursor="pointer"
+                    />
+                )}
                 <LogOut onClick={() => logout()} open={menu} data-test="menu">
                     <p data-test="logout">Logout</p>
                 </LogOut>
-                <img onClick={() => setMenu(!menu)} data-test="avatar" />
+                <img src={user.user?.photo} onClick={() => setMenu(!menu)} data-test="avatar" alt="User Avatar" />
             </div>
         </HeaderStyle>
-    )
+    );
 }
 
 
+
+
 const HeaderStyle = styled.div`
-    width: 100vw;
-    background-color: #151515;
-    position: absolute;
-    top: 0;
-    left: 0;
-    height:72px;
-    padding-left: 10px;
+    position: fixed;
+    width: -webkit-fill-available;
+    margin: 0px auto;
+    background-color: rgb(21, 21, 21);
+    top: 0px;
+    left: 0px;
+    height: 72px;
+    padding-left: 20px;
     padding-right: 20px;
     display: flex;
+    -webkit-box-pack: justify;
     justify-content: space-between;
+    -webkit-box-align: center;
     align-items: center;
     z-index: 3;
         h1 {
